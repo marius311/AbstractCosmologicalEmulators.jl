54725a5038d8d6bf051f45aea28db223cc0bac5f
Marco Bonici <bonici.marco@gmail.com>
2023-03-09 00:49:54 +0100

    Added first basic commands

---
+++ src/AbstractEmulator.jl
@@ -0,0 +1,35 @@
+module AbstractEmulator
+
+using Base: @kwdef
+using LoopVectorization
+using SimpleChains
+#TODO: add the FluxEmulators
+export AbstractTrainedEmulators, SimpleChainsEmulator
+export maximin_input!, inv_maximin_output!, run_emulator
+
+function maximin_input!(x, in_MinMax)
+    for i in eachindex(x)
+        x[i] -= in_MinMax[i,1]
+        x[i] /= (in_MinMax[i,2]-in_MinMax[i,1])
+    end
+end
+
+function inv_maximin_output!(x, out_MinMax)
+    for i in eachindex(x)
+        x[i] *= (out_MinMax[i,2]-out_MinMax[i,1])
+        x[i] += out_MinMax[i,1]
+    end
+end
+
+abstract type AbstractTrainedEmulators end
+
+@kwdef mutable struct SimpleChainsEmulator <: AbstractTrainedEmulators
+    Architecture
+    Weights
+end
+
+function run_emulator(input, trained_emulator::SimpleChainsEmulator)
+    return trained_emulator.Architecture(input, trained_emulator.Weights)
+end
+
+end # module AbstractEmulator
