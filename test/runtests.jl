be90ab7f7f162c4b3621e7db83e1d5cd35d3fa07
Marco Bonici <bonici.marco@gmail.com>
2023-03-09 07:09:08 +0100

    Fixing unittests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,19 +5,18 @@ using Test
 
 m = 100
 n = 200
-a = -2
-b = 5
-InMinMax = hcat(a .*ones(m), b .*ones(m))
+InMinMax = hcat(zeros(m), ones(m))
 
 @testset "AbstractEmulators test" begin
-    x = rand(a:b, m)
-    y = rand(a:b, m, n)
+    x = rand(m)
+    y = rand(m, n)
+
     X = deepcopy(x)
     Y = deepcopy(y)
     maximin_input!(x, InMinMax)
     maximin_input!(y, InMinMax)
-    @test any(x .>=0 .& x .<=1)
-    @test any(y .>=0 .& x .<=1)
+    @test any(x .>=0 .&& x .<=1)
+    @test any(y .>=0 .&& y .<=1)
     inv_maximin_output!(x, InMinMax)
     inv_maximin_output!(y, InMinMax)
     @test any(x .== X)
