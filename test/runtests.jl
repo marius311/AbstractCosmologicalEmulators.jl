000346aadb5bd10cd9e183d7d8ca19e18da35a5f
Marco Bonici <bonici.marco@gmail.com>
2023-03-09 06:48:13 +0100

    Fixing and adding tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,23 @@ using SimpleChains
 using Static
 using Test
 
-n = 100
-m = 200
+m = 100
+n = 200
 a = -2
 b = 5
-InMinMax = hcat(a .*ones(n), b .*ones(n))
+InMinMax = hcat(a .*ones(m), b .*ones(m))
 
 @testset "AbstractEmulators test" begin
-    x = randn(a:b, n)
+    x = rand(a:b, m)
+    y = rand(a:b, m, n)
+    X = deepcopy(x)
+    Y = deepcopy(y)
     maximin_input!(x, InMinMax)
+    maximin_input!(y, InMinMax)
     @test any(x .>=0 .& x .<=1)
+    @test any(y .>=0 .& x .<=1)
+    inv_maximin_output!(x, InMinMax)
+    inv_maximin_output!(y, InMinMax)
+    @test any(x .== X)
+    @test any(y .== Y)
 end
