186693234d46654ab2a37a4010e0c849dcb5aa9a
Marco Bonici <58727599+marcobonici@users.noreply.github.com>
2023-08-09 21:56:15 +0200

    Nn initialization (#10)
    * Added tagbot
    
    * Modify CI
    
    * Fix
    
    * Fix ci, addying tests on 1.10
    
    * Add instantiate NN interface
    
    * Fixing wrong naming
    
    * Added dict in the test directory
    
    * Fixed number of layers test dict
    
    * Advancing version
    
    * Added complete coverage
    
    * Fixed error in unit tests
    
    ---------
    
    Co-authored-by: Marco Bonici <bonici.marco@gmail.com>

--- src/AbstractCosmologicalEmulators.jl
+++ src/AbstractCosmologicalEmulators.jl
@@ -2,9 +2,8 @@ module AbstractCosmologicalEmulators
 
 using Base: @kwdef
 using SimpleChains
-#TODO: add the FluxEmulators
 export AbstractTrainedEmulators, SimpleChainsEmulator
-export maximin_input!, inv_maximin_output!, run_emulator
+export maximin_input!, inv_maximin_output!, run_emulator, instantiate_NN
 
 function maximin_input!(input::Vector, in_MinMax)
     for i in eachindex(input)
@@ -47,4 +46,28 @@ function run_emulator(input, emulator::SimpleChainsEmulator)
     return emulator.Architecture(input, emulator.Weights)
 end
 
-end # module AbstractEmulator
+function _create_layer(input_dict::Dict)
+    if input_dict["activation_function"] == "tanh"
+        act_func = SimpleChains.tanh
+    elseif input_dict["activation_function"] == "relu"
+        act_func = SimpleChains.relu
+    else
+        error("Error in the Activation Function! You choose "*
+        string(input_dict["activation_function"])*" which we do not support!")
+    end
+    return TurboDense(act_func, Int(input_dict["n_neurons"]))
+end
+
+function _create_hidden_layers_tuple(input_dict::Dict)
+    n_hidden_layers = input_dict["n_hidden_layers"]
+    hid_lay_tuple = (_create_layer(input_dict["layers"]["layer_"*string(i)]) for i in 1:n_hidden_layers)
+    return hid_lay_tuple
+end
+
+function instantiate_NN(input_dict::Dict)
+    hidden_layer_tuple = _create_hidden_layers_tuple(input_dict)
+    return SimpleChain(static(input_dict["n_input_features"]), hidden_layer_tuple...,
+    TurboDense(identity, input_dict["n_output_features"]))
+end
+
+end # module AbstractCosmologicalEmulators
